08e36726d3d03eaa83042ed3888143b2ddf63206
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-15 16:08:53 -0400

    Add files via upload

---
+++ Assignment7_1.java
@@ -0,0 +1,11 @@
+package Session7;
+
+public class Assignment7_1 {
+
+	public static void main(String[] args) {
+
+		int c = 10; 
+	    String str = String.valueOf(c); 
+	    System.out.println("String str3: " + str); 
+	  } 
+	}
